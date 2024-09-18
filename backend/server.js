43705c553bb21fb19919f4972a74e93483e60c43
Sai Prasad <saiprasadpaloju2gmail.com>
2024-09-18 18:56:37 +0530

    Connected mongodb atlas

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,22 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5500;
 
-const MONGO_URI = 'mongodb://localhost:27017/Travel_Tales';
+const MONGO_URI = process.env.MONGO_URL;
+console.log(MONGO_URI)
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-mongoose.connect(MONGO_URI)
+mongoose.connect(MONGO_URI, {
+    tls: true,
+})
     .then(() => console.log('MongoDB connected'))
     .catch((err) => console.error('MongoDB connection error:', err));
 
